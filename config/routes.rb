fe8170d894ff09996b2bba610930c9a3027d17cf
Mahmoud Bakr <mbakr6821@gmail.com>
2021-04-27 11:02:18 +0200

    setting up profiles and home files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :profiles
   resources :admins, only: %i[show edit update destroy]
   get '/sign_up', to: 'admins#new'
   post '/sign_up', to: 'admins#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  root 'admins#new'
+  root 'home#index'
 end
+ 
